c6f179429b38ef88d56996f30a52d0f65344b8aa
Daniel Marchi <marchi775@gmail.com>
2019-03-31 20:36:44 -0300

    First commit ;3

---
+++ index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text, AppRegistry } from 'react-native';
+
+import Header from './src/components/Header'
+
+const App = () => (
+  <Header />
+);
+
+AppRegistry.registerComponent('albums', () => App);
